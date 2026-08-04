6e2bb474f6a1d9a4ded26dbd949078c1ff291051
Bofah Emmanuel <mj3771910@gmail.com@gmail.com>
2026-08-04 16:13:21 +0000

    Prepare for Render deployment

--- frontend/src/pages/AdminQuestions.jsx
+++ frontend/src/pages/AdminQuestions.jsx
@@ -100,8 +100,9 @@ const AdminQuestions = () => {
 
  const fetchSubjects = async (courseId = "") => {
   try {
-    let url = "/subjects/admin/all";
-    if (courseId) url = `/subjects/admin/all?course=${courseId}`;
+    // Use the regular subjects endpoint instead of admin/all
+    let url = "/subjects";
+    if (courseId) url = `/subjects?course=${courseId}`;
     const res = await axios.get(url);
     setSubjects(res.data);
   } catch (err) {
